007b4655ec27e19f081eedd5e712662a51ea6106
mawenlong <15061886113@163.com>
2019-01-10 19:05:03 +0800

    socket通信

--- src/main/java/com/mwl/ch02/ServerBoot.java
+++ src/main/java/com/mwl/ch02/ServerBoot.java
@@ -3,13 +3,14 @@ package com.mwl.ch02;
 /**
  * @author mawenlong
  * @date 2019/01/09
- *
+ * <p>
  * 不使用netty写客户端与服务端
  */
 public class ServerBoot {
-    private static final int PORT=8000;
+    private static final int PORT = 8000;
 
     public static void main(String[] args) {
-
+        Server server = new Server((PORT));
+        server.start();
     }
 }
